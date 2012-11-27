ba5875a81210769325991cba63e9ba7023b93179
Rovin Varshney <rovin100@gmail.com>
2012-11-27 21:39:48 +0530

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pcloud::Application.config.secret_token = 'b33f89080d7f5f4704dc749d726787c4e4eab0c6ed19ce7f1fa0a010a9063e1ac66c7bb3f8cc023860663881da45e1cbb821c30b5f88b151b6eb2f9dd2da140e'
